18d628063f35c7b0c0f82fea8c4f65e5f3510402
ck100100 <33187318+ck100100@users.noreply.github.com>
2025-05-16 16:29:09 +0300

    created create flight page

--- gui/java gui playground/src/Pages/Flights/PageCreateFlight.java
+++ gui/java gui playground/src/Pages/Flights/PageCreateFlight.java
@@ -1,21 +1,30 @@
 package Pages.Flights;
+import components.FormDateTimeInput;
 import components.FormInput;
 import components.FormLabel;
 import components.MainWindow;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PageCreateFlight extends MainWindow {
-    public PageCreateFlight(String title) {
-        super(title);
+    public PageCreateFlight() {
+        super("Create Flight");
     }
 
     @Override
     protected JPanel generateBody() {
         var panel = new JPanel();
-        panel.add(new FormLabel("Hi"));
-        var input1 = new FormInput("Hello", true, "");
-        panel.add(input1);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        FormInput flightNumberInput = new FormInput("Flight Number", true, "");
+        var departureDateInput = new FormDateTimeInput("Departure Time", true, 00, 00, 01, 01, 25);
+        var arrivalDateInput = new FormDateTimeInput("Arrival Time", true, 00, 00, 01, 01, 25);
+
+        panel.add(flightNumberInput);
+        panel.add(departureDateInput);
+        panel.add(arrivalDateInput);
+        
 
         return panel;
     }
