e5539df7f5796df7a426b7c807c019014fda9b11
ck100100 <33187318+ck100100@users.noreply.github.com>
2025-05-21 18:03:38 +0300

    ability to add multiple pilots/flightattendants

--- gui/java gui playground/src/Pages/Flights/PageCreateFlight.java
+++ gui/java gui playground/src/Pages/Flights/PageCreateFlight.java
@@ -4,9 +4,13 @@ import components.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
+
+import static javax.swing.BorderFactory.createEmptyBorder;
 
 public class PageCreateFlight extends MainWindow {
     public PageCreateFlight() {
@@ -34,10 +38,10 @@ public class PageCreateFlight extends MainWindow {
         arrivalAirport.setAlignmentX(Component.LEFT_ALIGNMENT);
         var airplaneID = new FormDropdown("Airplane", airplaneList);
         airplaneID.setAlignmentX(Component.LEFT_ALIGNMENT);
-        var pilotID = new FormDropdown("Pilot", airplaneList);
-        pilotID.setAlignmentX(Component.LEFT_ALIGNMENT);
-        var flightAttendantID = new FormDropdown("Flight Attendant", airplaneList);
-        flightAttendantID.setAlignmentX(Component.LEFT_ALIGNMENT);
+        var pilotIDs = new FormDropdownMultiSelect("Pilot", airplaneList, null);
+        pilotIDs.setAlignmentX(Component.LEFT_ALIGNMENT);
+        var flightAttendantIDs = new FormDropdownMultiSelect("Flight Attendant", airplaneList, null);
+        flightAttendantIDs.setAlignmentX(Component.LEFT_ALIGNMENT);
         var menuID = new FormDropdown("Menu", airplaneList);
         menuID.setAlignmentX(Component.LEFT_ALIGNMENT);
 
@@ -48,19 +52,20 @@ public class PageCreateFlight extends MainWindow {
         panel.add(departureAirport);
         panel.add(arrivalAirport);
         panel.add(airplaneID);
-        panel.add(pilotID);
-        panel.add(flightAttendantID);
+        panel.add(pilotIDs);
+        panel.add(flightAttendantIDs);
         panel.add(menuID);
 
         panel.add(renderViewActionButtons());
 
         // Scroll Functionality
         var scrollPane = new JScrollPane(panel);
+        scrollPane.setBorder(createEmptyBorder());
         var wrapperPanel = new JPanel();
         wrapperPanel.setLayout(new BorderLayout());
         wrapperPanel.add(scrollPane, BorderLayout.CENTER);
 
-        return panel;
+        return wrapperPanel;
     }
 
     private JPanel renderViewActionButtons() {
@@ -68,16 +73,32 @@ public class PageCreateFlight extends MainWindow {
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.setLayout(new FlowLayout());
 
-        var editBtn = ButtonFactory.primary("Edit");
-        var rescheduleBtn = ButtonFactory.primary("Reschedule");
-        var cancelFlightBtn = ButtonFactory.primary("Cancel Flight");
         var cancelBtn = ButtonFactory.primary("Cancel");
+        cancelBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onSelectCancel();
+            }
+        });
+        var saveBtn = ButtonFactory.primary("Save");
+        saveBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onSelectSave();
+            }
+        });
 
-        panel.add(editBtn);
-        panel.add(rescheduleBtn);
-//        panel.add(cancelFlightBtn);
         panel.add(cancelBtn);
+        panel.add(saveBtn);
 
         return panel;
+    }
+
+    private void onSelectCancel() {
+        closeWindow();
+    }
+
+    private void onSelectSave() {
+        // fix this
     }
 }
