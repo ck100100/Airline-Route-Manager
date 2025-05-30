a072f0bb5263c61b739b0577dcf1adb96f220151
ck100100 <33187318+ck100100@users.noreply.github.com>
2025-05-30 09:33:37 +0300

    added main page

--- gui/java gui playground/src/Main.java
+++ gui/java gui playground/src/Main.java
@@ -1,14 +1,4 @@
-import Controllers.ControllerAirplane;
-import Controllers.ControllerFoodMenu;
-import Controllers.ControllerMenuItem;
-import Pages.Flights.PageCreateFlight;
-import Pages.Flights.PageViewFlightList;
-import Pages.Menu.PageInsertMenuItem;
-import Pages.Menu.PageMenuCreation;
-import Pages.PilotReports.PageAvailablePlanes;
-import Pages.PilotReports.PagePlaneReport;
-import Pages.PilotReports.PageRecentFlight;
-import components.MainWindow;
+import Pages.PageMain;
 import Controllers.*;
 
 import javax.swing.*;
@@ -18,14 +8,8 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // Define controllers
-                var controllerFlight = new ControllerFlight();
-                var controllerAirplane = new ControllerAirplane();
-                var controllerFoodMenu = new ControllerFoodMenu();
-                var controllerAirport = new ControllerAirport();
-                Airline airline = new Airline(controllerFlight, controllerAirplane, controllerFoodMenu, controllerAirport);
-
-                var main = new PageViewFlightList(airline);
+                Airline airline = new Airline();
+                var main = new PageMain(airline);
                 main.show();
             }
         });
